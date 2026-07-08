c56f2bdffa0b7aa709825f2f2cfcc1cc18dd2a71
Maurizio <99482352+wbtcpip2@users.noreply.github.com>
2026-07-08 17:19:33 +0200

    Fix OpenSSL version directory in build script

--- ci/build-mingw64.sh
+++ ci/build-mingw64.sh
@@ -178,7 +178,7 @@ _dav1d_mark=lib/libdav1d.dll.a
 
 _openssl () {
     local ver=3.3.1
-    gettar "https://github.com/openssl/openssl/archive/refs/tags/openssl-${ver}.tar.gz" openssl-${ver}
+    gettar "https://github.com/openssl/openssl/archive/refs/tags/openssl-${ver}.tar.gz" openssl-openssl-${ver}
     builddir openssl-${ver}
 
     # OpenSSL build for MinGW
